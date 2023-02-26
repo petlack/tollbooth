ccc51a1e8ba41583d8f1031ef466ed2a4b568d41
peter@output.sk <peter@output.sk>
2023-02-27 01:06:09 +0200

    refactor: remove unused code

--- src/redis.ts
+++
@@ -1,6 +0,0 @@
-import { Redis } from 'ioredis';
-class CustomRedisClient extends Redis {
-  incrByHashValue(key1: string, key2: string): Promise<number> {
-    return this.sendCommand('incrByValue', [key1, key2]);
-  }
-}
