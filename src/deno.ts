bff0796c01696f26a4dff6a716facca9df0ca1db
peter@output.sk <peter@output.sk>
2023-03-04 16:13:02 +0200

    style: prettify

--- src/deno.ts
+++ src/deno.ts
@@ -3,7 +3,10 @@ import { CommandArg, Reply } from './types';
 type Fn<I, O> = (...args: I[]) => O;
 type CmdFn<I, O> = (cmd: string, ...args: I[]) => O;
 
-type SendCommandFn<I extends CommandArg, T, R extends Reply> = (conn: T, args: I[]) => R | Promise<R>;
+type SendCommandFn<I extends CommandArg, T, R extends Reply> = (
+  conn: T,
+  args: I[],
+) => R | Promise<R>;
 
 export function mapFunctionsToKeys<I, O, T extends Fn<I, O>, K extends string>(
   fn: CmdFn<I, O>,
@@ -18,7 +21,10 @@ export function RedisFromSendCommand<I extends CommandArg, T, R extends Reply>(
   conn: T,
   sendCommand: SendCommandFn<I, T, R>,
 ) {
-  return mapFunctionsToKeys((cmd, ...args: I[]) => {
-    return sendCommand(conn, [cmd.toUpperCase() as I, ...args]);
-  }, ['eval', 'hset', 'hget', 'hdel', 'keys', 'del']);
+  return mapFunctionsToKeys(
+    (cmd, ...args: I[]) => {
+      return sendCommand(conn, [cmd.toUpperCase() as I, ...args]);
+    },
+    ['eval', 'hset', 'hget', 'hdel', 'keys', 'del'],
+  );
 }
