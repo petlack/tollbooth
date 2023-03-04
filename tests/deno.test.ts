4263d76ee0a9e8a21a23fc427589d07369466ac8
peter@output.sk <peter@output.sk>
2023-03-04 16:37:19 +0200

    fix: deno ts error

--- tests/deno.test.ts
+++ tests/deno.test.ts
@@ -1,6 +1,6 @@
 import { RedisFromSendCommand } from '../src/deno';
 
-function sendCommand(conn: string, args: (string | number)[]) {
+async function sendCommand(conn: string, args: (string | number)[]) {
   return [conn, ...args];
 }
 
@@ -8,11 +8,11 @@ const redis = RedisFromSendCommand('conn', sendCommand);
 
 describe('RedisFromSendCommand', () => {
   it('hget', () => {
-    expect(redis.hget('foo', 'bar')).toEqual(['conn', 'HGET', 'foo', 'bar']);
+    expect(redis.hget('foo', 'bar')).resolves.toEqual(['conn', 'HGET', 'foo', 'bar']);
   });
 
   it('eval', () => {
-    expect(redis.eval('return KEYS[0] .. ARGV[1]', 1, 'foo', 'bar')).toEqual([
+    expect(redis.eval('return KEYS[0] .. ARGV[1]', 1, 'foo', 'bar')).resolves.toEqual([
       'conn',
       'EVAL',
       'return KEYS[0] .. ARGV[1]',
